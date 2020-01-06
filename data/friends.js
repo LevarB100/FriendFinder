df3df43acbf9e3ae8bcba286ea6ee7354b1d0d88
Levar Barrett <emaillevar@gmail.com>
2020-01-06 10:04:16 -0500

    routes for imgages

--- data/friends.js
+++ data/friends.js
@@ -1,52 +1,52 @@
 const friends = [
   {
     name: "Ahmed",
-    photo: "../data/images1/dude1.jpg",
+    photo: "./images1/dude1.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: "tom",
-    photo: "../data/images1/dude2.jpg",
+    photo: "./images1/dude2.jpg",
     scores: [3, 1, 4, 2, 5, 1, 2, 5, 1, 3]
   },
   {
     name: "John",
-    photo: "../data/images1/dude3.jpg",
+    photo: "./images1/dude3.jpg",
     scores: [1, 4, 4, 3, 5, 5, 2, 1, 4, 1]
   },
   {
     name: "Mike",
-    photo: "../data/images1/dude4.jpg",
+    photo: "./images1/dude4.jpg",
     scores: [1, 1, 3, 2, 1, 1, 2, 5, 2, 1]
   },
   {
     name: "Rob",
-    photo: "../data/images1/dude5.jpg",
+    photo: "./images1/dude5.jpg",
     scores: [5, 5, 5, 4, 5, 5, 5, 5, 4, 1]
   },
   {
     name: "Terry",
-    photo: "../data/images1/dude6.jpg",
+    photo: "./images1/dude6.jpg",
     scores: [2, 1, 2, 1, 2, 1, 2, 2, 4, 1]
   },
   {
     name: "Marshall",
-    photo: "../data/images1/dude7.jpg",
+    photo: "./images1/dude7.jpg",
     scores: [1, 5, 4, 1, 5, 1, 2, 2, 1, 1]
   },
   {
     name: "Antonio",
-    photo: "../data/images1/dude8.jpg",
+    photo: "./images1/dude8.jpg",
     scores: [1, 1, 5, 5, 5, 1, 2, 5, 1, 1]
   },
   {
     name: "Sean",
-    photo: "../data/images1/dude9.jpg",
+    photo: "./images1/dude9.jpg",
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   {
     name: "Lit",
-    photo: "../data/images1/dude10.jpg",
+    photo: "./images1/dude10.jpg",
     scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   }
 ];
