bee9589529b2ad73d36677612823046cc0aa65a9
Levar Barrett <emaillevar@gmail.com>
2020-01-13 17:50:35 -0500

    update

--- public/main.js
+++ public/main.js
@@ -32,7 +32,7 @@ $("#submit").on("click", function() {
   $.post("/api/friends", data, function(data) {
     console.log(data);
 
-    $(".modal").show();
+    $(".mdal").show();
     $("#resultsname").text(data.name);
     $("#resultspic").attr("src", data.photo);
   });
